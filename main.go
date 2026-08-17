286a10ae5418d42a5e6706be426efbd5342fcf68
Duc Huy LY <lyduchuy@proton.me>
2026-08-17 12:44:38 +0200

    log number of hits on app

--- main.go
+++ main.go
@@ -1,24 +1,47 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"sync/atomic"
 )
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func okResponseHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(200)
+	w.Write([]byte("OK"))
+}
+
+func (cfg *apiConfig) requestLogger(w http.ResponseWriter, r *http.Request) {
+	hits := fmt.Sprintf("Hits: %v", cfg.fileserverHits.Load())
+	w.Write([]byte(hits))
+}
 
 func main() {
 	const fileRootPath = "."
 	const port = "8080"
-
-	mux := http.NewServeMux()
-	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir(fileRootPath))))
-
-	myPersonalHandlerFunc := func(writer http.ResponseWriter, req *http.Request) {
-		writer.Header().Set("Content-Type", "text/plain; charset=utf-8")
-		writer.WriteHeader(200)
-		writer.Write([]byte("OK"))
+	apiCfg := &apiConfig{
+		fileserverHits: atomic.Int32{},
 	}
 
-	mux.HandleFunc("/healthz", myPersonalHandlerFunc)
+	mux := http.NewServeMux()
+	handler := http.StripPrefix("/app", http.FileServer(http.Dir(fileRootPath)))
+	mux.Handle("/app/", apiCfg.middlewareMetricsInc(handler))
+
+	mux.HandleFunc("/healthz", http.HandlerFunc(okResponseHandler))
+	mux.HandleFunc("/metrics", http.HandlerFunc(apiCfg.requestLogger))
 
 	server := &http.Server{
 		Handler: mux,
